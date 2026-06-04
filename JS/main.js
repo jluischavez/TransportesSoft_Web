34feac3ab7e435012fcb9805aa5ac2ee625c525e
José Luis Chavez Moreno <joseluischavezmoreno52@gmail.com>
2026-06-04 13:29:12 -0700

    asi mero

--- JS/main.js
+++ JS/main.js
@@ -15,7 +15,7 @@ document.getElementById("btnLogin").addEventListener("click", async () => {
     }
 
     try {
-        const response = await fetch("https://transportessoftwebapi.azurewebsites.netUsuariosCat/login", {
+        const response = await fetch("https://transportessoftwebapi.azurewebsites.net/UsuariosCat/login", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ nombreUsuario: usuario, contrasena: password })
